51a1c86d373dd0a054a52314183d6aff8dc955a7
DariaKalinichenko <kddm2015@gmail.com>
2020-09-29 09:33:19 +0300

    Edited pep8

--- tests/settings_qa.py
+++ tests/settings_qa.py
@@ -1,8 +1,8 @@
-from api_yamdb.settings import *  # noqa: E501
+from api_yamdb.settings import *  # noqa: F403
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa: E501
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # noqa: E5405
     }
 }
