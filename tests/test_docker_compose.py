c59acce1cd49dfe1a48748fb6dbe758b6925d3bb
DariaKalinichenko <kddm2015@gmail.com>
2020-10-04 12:00:59 +0300

    Added nginx

--- tests/test_docker_compose.py
+++ tests/test_docker_compose.py
@@ -8,7 +8,7 @@ class TestDockerfileCompose:
 
     def test_dockerfile_compose(self):
         try:
-            with open(f'{os.path.join(settings.BASE_DIR, "docker-compose.yaml")}', 'r') as f:  # noqa: E501
+            with open(f'{os.path.join(settings.BASE_DIR, "docker-compose.txt")}', 'r') as f:  # noqa: E501
                 docker_compose = f.read()
         except FileNotFoundError:
             assert False, 'Проверьте, что добавили файл docker_compose.yaml'
@@ -16,4 +16,4 @@ class TestDockerfileCompose:
         assert re.search(r'image:\s+postgres:latest', docker_compose), \
             'Проверьте, что добавили образ postgres:latest в файл docker_compose'  # noqa: E501
         assert re.search(r'build:\s+\.', docker_compose), \
-            'Проверьте, что добавили сборку контейнера из Dockerfile в файл docker_compose'  # noqa: E501
+            'Проверьте, что добавили сборку контейнера из Dockerfile.txt в файл docker_compose'  # noqa: E501
